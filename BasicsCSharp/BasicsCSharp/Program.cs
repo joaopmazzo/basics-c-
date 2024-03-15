82957695fcae998db2073b9b040a0abe77ced63a
João Paulo Mazzo <mazzojp@gmail.com>
2024-03-14 21:59:52 -0300

    first commit

---
+++ BasicsCSharp/BasicsCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace BasicsCSharp;
+
+class Program
+{
+    enum DifficultyLevel
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2
+    }
+
+    static void Main()
+    {
+        string text = "O usuário {0} gosta do numero {1}";
+        string result = string.Format(text, "João Mazzo", 8);
+        Console.WriteLine(result);
+
+
+
+        DateOnly day = new DateOnly(2023, 12, 1);
+        string dayInText = day.ToString("dd MMMM yyyy", new CultureInfo("pt-BR"));
+        Console.WriteLine(dayInText);
+
+        DateTime dateAndTime = new DateTime(2023, 12, 1, 20, 07, 1);
+        DateTime dateAndTimeNow = DateTime.Now;
+        DateTime dateAndTimeUtc = DateTime.UtcNow;
+        DateTime dateToday = DateTime.Today;
+
+        DateTime datePlusDays = dateAndTimeNow.AddDays(2);
+
+
+
+        DifficultyLevel difficultyLevel = DifficultyLevel.Low;
+        int difficultyLevelInt = (int)difficultyLevel;
+        Console.WriteLine(difficultyLevel);
+        Console.WriteLine(difficultyLevelInt);
+    }
+}
